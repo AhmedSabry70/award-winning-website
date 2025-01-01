f508280ada8f079d385438659c9b350a753db527
Ahmed Sabry <infosabry88@gmail.com>
2025-01-01 14:22:56 +0200

    setup project , assets and styles

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+     <section className="min-h-dvh w-screen bg-violet-300/20"/>
+     <section className="min-h-dvh w-screen bg-blue-300/20"/>
+    
+     <section className="min-h-dvh w-screen bg-purple-300/20"/>
+    </main>
+  )
+}
+
+export default App
